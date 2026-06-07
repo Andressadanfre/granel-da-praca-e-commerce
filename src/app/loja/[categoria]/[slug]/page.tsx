5b69660f25593f8318c0c3b56bf8e00443e89601
Andressadanfre <danfre38@gmail.com>
2026-06-07 09:31:42 -0300

    feat(pdp): integrar ProductDetailHero, PdpActions, ProductDescription e RelatedProducts no page.tsx

--- src/app/loja/[categoria]/[slug]/page.tsx
+++ src/app/loja/[categoria]/[slug]/page.tsx
@@ -1,6 +1,10 @@
 ﻿import { notFound } from 'next/navigation'
 import { cache } from 'react'
 import { getProductDetail } from '@/lib/products/product-detail'
+import { ProductDetailHero } from '@/components/pdp/ProductDetailHero'
+import { PdpActions } from '@/components/pdp/PdpActions'
+import { ProductDescription } from '@/components/pdp/ProductDescription'
+import { RelatedProducts } from '@/components/pdp/RelatedProducts'
 
 const getCachedProduct = cache(getProductDetail)
 
@@ -30,13 +34,23 @@ export default async function ProductDetailPage({ params }: Props) {
       <div className="max-w-container mx-auto px-s5 xl:px-0 pt-8 pb-16">
         <div className="grid lg:grid-cols-[1fr_400px] gap-8 lg:gap-12 items-start">
           <div>
-            {/* ProductDetailHero - Passo 3 */}
+            <ProductDetailHero produto={produto} />
+            <ProductDescription description={produto.description} />
           </div>
           <aside className="lg:sticky lg:top-[88px]">
-            {/* PdpActions - Passo 4 */}
+            <PdpActions
+              productId={produto.id}
+              productType={produto.product_type}
+              stockStatus={produto.stock_status}
+            />
           </aside>
         </div>
-        {/* RelatedProducts - Passo 6 */}
+        <div className="mt-16">
+          <RelatedProducts
+            categoryId={produto.category.id}
+            excludeId={produto.id}
+          />
+        </div>
       </div>
     </main>
   )
